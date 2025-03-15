d10491510c2770b21c3d20eaf406f40cf1b7d484
Nida Nurrohmah <60107104@udst.edu.qa>
2025-03-15 23:05:09 +0300

    Question Library Page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from models import db, Library
-
+from forms import QuestionForm
 import os
 
 app = Flask(__name__)
@@ -17,6 +17,16 @@ with app.app_context():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/question-library')
+def getLibrary():
+    library = Library.query.all()
+    return render_template('Library.html', library=library)
+
+@app.route('/api/question-library', methods=['GET'])
+def get_questions():
+    library = Library.query.all()
+    return jsonify([library.to_dict() for question in library])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
